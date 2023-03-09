b206855efa14d09ace37a2e183fc0869d36305d1
Matthew So <matthew.so@gatech.edu>
2023-03-09 16:55:00 -0500

    Convenience  script added to annotation script

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Usage: ./run.sh (review_source_folder_name) (sign)
+# Example: ./run.sh review_1 thankyou
+python3 fast_annotate.py -d ~/ServerData/sign_language_videos/$1 -o ~/ServerData/sign_language_videos/annotation_output -s $2
