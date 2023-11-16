b4123bcfff76b2a48e160239170dd88fce855a00
Bill Neubauer <wneubauer4@gatech.edu>
2023-11-16 14:56:39 -0500

    Created phrase review branch

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Usage: ./run.sh (review_source_folder_name) (sign)
-# Example: ./run.sh no_mediapipe batch_10
-python3 fast_annotate.py -d ~/ServerData/sign_language_videos/review_sets/review_mediapipe/ -g $1 -o ~/ServerData/sign_language_videos/mediapipe_annotation_output/$1 -s $2
+#THIS IS TEMPORARY SWITCH TO A DIFFERENT BRANCH
+# Example: ./run.sh review_1 batch_10
+python3 fast_annotate.py -d ~/ServerData/sign_language_videos/review_sets/review_phrase/ -g $1 -o ~/ServerData/sign_language_videos/annotation_output_phrases/$1 -s $2
