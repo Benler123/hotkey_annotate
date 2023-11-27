d204f50bd95b3129c96e5a74b8052fcacf97d0d8
Bill Neubauer <wneubauer4@gatech.edu>
2023-11-27 00:07:47 -0500

    Created Misclassfied Review branch

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Usage: ./run.sh (review_source_folder_name) (sign)
 # Example: ./run.sh review_1 thankyou
-python3 fast_annotate.py -d ~/ServerData/sign_language_videos/review_sets/ -g $1 -o ~/ServerData/sign_language_videos/garbage_output -s $2
+python3 fast_annotate.py -d ~/ServerData/sign_language_videos/review_sets/review_misclassified -g $1 -o ~/ServerData/sign_language_videos/annotation_output_misclassified -s $2
