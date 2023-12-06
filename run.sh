878c85df3262ad328cc540173dccbc7ce0c6d018
Bill Neubauer <wneubauer4@gatech.edu>
2023-12-05 19:18:41 -0500

    Modified run.sh for the second pass

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Usage: ./run.sh (review_source_folder_name) (sign)
 # Example: ./run.sh review_1 thankyou
-python3 fast_annotate.py -d ~/ServerData/sign_language_videos/review_sets/review_misclassified -g $1 -o ~/ServerData/sign_language_videos/annotation_output_misclassified -s $2
+python3 fast_annotate.py -d ~/ServerData/sign_language_videos/review_sets/review_misclassified_pass_2/ -g $1 -o ~/ServerData/sign_language_videos/annotation_output_misclassified_pass_2/variant/ -s $2
